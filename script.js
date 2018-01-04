7bb2fef3566e89ed71cf79492adfe8469faa4602
Joseph Davis <JNDavis1@dstsystems.com>
2018-01-04 14:01:33 -0600

    Add Google Analytics tag and use Materialize

--- script.js
+++ script.js
@@ -1,57 +1,48 @@
-var users = ["ESL_SC2", "OgamingSC2", "freecodecamp", "storbeck", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404", "cretetion", "habathcx", "dreamleague"];
-var name, game, logo, description, status, url, content;
+const users = ["ESL_SC2", "OgamingSC2", "freecodecamp", "storbeck", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404", "cretetion", "habathcx", "dreamleague"];
+const defaultLogo = "https://res.cloudinary.com/mrwhitenerdy/image/upload/v1478309343/logo_vb0knh.png";
 
-$(document).ready(function () {
-    setInterval(getData(), 30000);
-});
+for (let i = 0; i < users.length; i++) {
+  let notFound = false;
+  let status, bgColor;
 
-function getData() {
-    users.forEach(function (user) {
-        $.ajax({
-            url: "https://wind-bow.gomix.me/twitch-api/streams/" + user,
-            dataType: "jsonp",
-            contentType: "application/json",
-            success: function (data) {
-                if (data.stream === null) {
-                    game = "Offline";
-                    status = "off";
-                } else if (data.stream === undefined) {
-                    game = "Account Closed";
-                    status = "off";
-                } else {
-                    game = data.stream.game;
-                    status = "on";
-                }
-            }
-        });
-        $.ajax({
-            url: "https://wind-bow.gomix.me/twitch-api/channels/" + user,
-            dataType: "jsonp",
-            contentType: "application/json",
-            success: function (data) {
-                if (data.logo != null) {
-                    logo = data.logo;
-                } else {
-                    logo = "https://res.cloudinary.com/mrwhitenerdy/image/upload/v1478309343/logo_vb0knh.png";
-                }
+  fetchJsonp(`https://wind-bow.gomix.me/twitch-api/users/${users[i]}`)
+  .then((res) => res.json())
+  .then((userData) => {
+    fetchJsonp(`https://wind-bow.gomix.me/twitch-api/channels/${users[i]}`)
+    .then((res) => res.json())
+    .then((channelData) => {
+      console.log(userData);
+      console.log(channelData);
 
-                if (data.display_name != null) {
-                    name = data.display_name;
-                } else {
-                    name = user;
-                }
+      if (channelData.error) {
+        notFound = true;
+      }
 
-                if (status == "off") {
-                    description = "";
-                } else {
-                    description = data.status;
-                }
+      if (notFound || channelData.status === null) {
+        status = 'Offline';
+        bgColor = 'light-blue lighten-3';
+      } else {
+        status = 'Online: ' + channelData.status;
+        bgColor = 'green lighten-3';
+      }
 
-                url = "https://twitch.tv/" + user;
+      let template = `
+        <div class="card-panel ${bgColor}">
+          <div class="row valign-wrapper">
+            <div class="col s2">
+              <img src="${userData.logo ? userData.logo : defaultLogo}" alt="" class="circle responsive-img">
+            </div>
+            <div class="col s10">
+              <h3><a href="${channelData.url}" target="_blank">${userData.display_name ? userData.display_name : users[i]}</a></h3>
+              <h5>${status}</h5>
+            </div>
+          </div>
+        </div>
+      `;
 
-                content = '<div class="row"><div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"><img src="' + logo + '" class="img-responsive img-circle"></div><div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><a href="' + url + '" target="_blank">' + name + '</a></div><div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><span>' + game + '</span> <span>' + description + '</span></div></div>';
-                $(".container").append(content);
-            }
-        });
-    });
+      document.querySelector('.output').innerHTML += template;
+    })
+    .catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
 }
